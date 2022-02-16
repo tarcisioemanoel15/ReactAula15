749c0b3bad57fa82e744c14dd5c9185ea8542dd9
tarcisio <tarcisioemanoel10031@gmail.com>
2022-02-15 21:58:00 -0300

    adicionando cores globais

---
+++ src/config/colors.js
@@ -0,0 +1,7 @@
+export const primaryColor = '#c3073f';
+export const primaryDarkColor = '#1A1A1D';
+
+export const successColor = '#0197f6';
+export const infoColor = '#0197f6';
+export const errorColor = '#f2af29';
+export const arningColor = '#f2af29';
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -10,11 +10,9 @@ function App() {
         <small> oii mundo </small>
       </Title>
       <Paragrafo>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-        Rem voluptas magni culpa neque dolorum eum veritatis veniam consequuntur,
-        qui perferendis eius, ducimus, quo facilis? Eos delectus rerum natus illum aut?
-
+        Lorem, ipsum dolor sit amet.
       </Paragrafo>
+      <button type="button">enviar</button>
     </Container>
   );
 }
